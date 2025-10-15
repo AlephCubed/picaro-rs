3b6b199d7fd61fdf226ec2dfed1748e4e3f7e992
AlephCubed <jothebro500@gmail.com>
2025-10-15 10:45:31 -0700

    Added simple benchmarks

---
+++ benches/decrypt.rs
@@ -0,0 +1,12 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use picaro_rs::Picaro;
+use std::hint::black_box;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let p = Picaro::new(1234);
+
+    c.bench_function("decrypt 1234", |b| b.iter(|| p.decrypt(black_box(1234))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
