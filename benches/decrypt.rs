4d55c397c8bad728480f3fba7328591fe3b7a45b
AlephCubed <jothebro500@gmail.com>
2025-11-09 13:47:35 -0800

    Updated benchmarks.

--- benches/decrypt.rs
+++ benches/decrypt.rs
@@ -2,18 +2,18 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use picaro_rs::Picaro;
 use std::hint::black_box;
 
-fn bench_decrypt<const MASKING_LEVEL: usize>(c: &mut Criterion) {
-    let p = Picaro::<MASKING_LEVEL>::new(1234);
-    c.bench_function(&format!("decrypt level {MASKING_LEVEL}"), |b| {
+fn bench_decrypt<const SHARE_COUNT: usize>(c: &mut Criterion) {
+    let mut p = Picaro::<SHARE_COUNT>::new_from_seed(1234, 1234);
+    c.bench_function(&format!("Decrypt ({SHARE_COUNT} shares)"), |b| {
         b.iter(|| p.decrypt(black_box(1234)))
     });
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    bench_decrypt::<0>(c);
     bench_decrypt::<1>(c);
     bench_decrypt::<2>(c);
     bench_decrypt::<3>(c);
+    bench_decrypt::<4>(c);
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/encrypt.rs
+++ benches/encrypt.rs
@@ -2,18 +2,18 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use picaro_rs::Picaro;
 use std::hint::black_box;
 
-fn bench_encrypt<const MASKING_LEVEL: usize>(c: &mut Criterion) {
-    let p = Picaro::<MASKING_LEVEL>::new(1234);
-    c.bench_function(&format!("encrypt level {MASKING_LEVEL}"), |b| {
+fn bench_encrypt<const SHARE_COUNT: usize>(c: &mut Criterion) {
+    let mut p = Picaro::<SHARE_COUNT>::new_from_seed(1234, 1234);
+    c.bench_function(&format!("Encrypt ({SHARE_COUNT} shares)"), |b| {
         b.iter(|| p.encrypt(black_box(1234)))
     });
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    bench_encrypt::<0>(c);
     bench_encrypt::<1>(c);
     bench_encrypt::<2>(c);
     bench_encrypt::<3>(c);
+    bench_encrypt::<4>(c);
 }
 
 criterion_group!(benches, criterion_benchmark);
